b1d4bfc56e36e31b12207e423cc85565ddf0510e
Inara <inarasalih2015@gmail.com>
2020-11-10 12:55:50 -0600

    Work with int

--- src/PracticeGitHub.java
+++ src/PracticeGitHub.java
@@ -1,5 +1,9 @@
 public class PracticeGitHub {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int n = 10;
+        int m = 15;
+        int sum = n + m;
+        System.out.println(sum);
     }
 }
